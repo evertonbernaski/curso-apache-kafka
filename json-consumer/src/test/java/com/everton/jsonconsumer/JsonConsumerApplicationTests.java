7bf63b454c65aec96b91df776a84cc08e85f68e7
Oliveira Bernaski <evertonbernaski@ext.cvccorp.com.br>
2023-02-02 14:09:08 -0300

    add json-consumer

---
+++ json-consumer/src/test/java/com/everton/jsonconsumer/JsonConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everton.jsonconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
