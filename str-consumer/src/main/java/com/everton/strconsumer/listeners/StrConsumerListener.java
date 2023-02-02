10d316d5c2e6fa1152b20f30b96541783bd2b565
Oliveira Bernaski <evertonbernaski@ext.cvccorp.com.br>
2023-02-02 10:47:28 -0300

    str- consumer

---
+++ str-consumer/src/main/java/com/everton/strconsumer/listeners/StrConsumerListener.java
@@ -0,0 +1,29 @@
+package com.everton.strconsumer.listeners;
+
+import com.everton.strconsumer.custom.StrConsumerCustomListener;
+import lombok.SneakyThrows;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Log4j2
+@Component
+public class StrConsumerListener {
+
+    @SneakyThrows
+    @StrConsumerCustomListener(groupId = "group-1")
+    public void create(String message) {
+        log.info("CREATE ::: Receive message {}", message);
+        throw new IllegalArgumentException("EXCEPTION...");
+    }
+
+    @StrConsumerCustomListener(groupId = "group-1")
+    public void log(String message) {
+        log.info("LOG ::: Receive message {}", message);
+    }
+
+    @KafkaListener(groupId = "group-2", topics = "str-topic", containerFactory = "validMessageContainerFactory")
+    public void history(String message) {
+        log.info("HISTORY ::: Receive message {}", message);
+    }
+}
